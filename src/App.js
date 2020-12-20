784915b306417c522c330f075b7aa60d8c3110f4
asfian99 <mk.asfian@gmail.com>
2020-12-20 22:52:24 +0700

    completed + dark mode working

--- src/App.js
+++ src/App.js
@@ -5,22 +5,63 @@ const API_KEY = "f94ae2cc03c6b614017e6b6263489f71";
 
 function App() {
   const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState({});
-  // console.log(city);
+  const [dark, setDark] = useState(false);
+  const [showData, setShowData] = useState(false);
+  const [weatherData, setWeatherData] = useState({
+    coord: {
+      lon: null,
+      lat: null,
+    },
+    weather: [
+      {
+        main: "",
+        description: "",
+      },
+    ],
+    main: {
+      temp: null,
+      humidity: null,
+    },
+    wind: {
+      speed: null,
+    },
+    name: "",
+  });
 
+  // console.log(dark);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
     );
     const newData = await data.json();
     setWeatherData(newData);
+    setShowData(true);
+    setCity("");
   };
 
   return (
-    <div className="">
+    <div className={dark ? "dark" : ""}>
       <div className="flex flex-col h-screen mx-auto text-gray-800 dark:text-gray-200 bg-gray-200 dark:bg-gray-800 justify-center items-center">
-        <h1 className="my-4 text-xl font-semibold">Hello Weather</h1>
+        <h1 className="text-2xl font-semibold">Hello Weather</h1>
+
+        {/* Dark mode switch */}
+        <div className="my-4 flex flex-row items-center">
+          Dark Mode :
+          <div
+            onClick={() => setDark(!dark)}
+            className={`m-2 w-12 h-8 flex items-center bg-gray-300 rounded-full p-1 ${
+              dark ? `bg-gray-600` : ``
+            }`}
+          >
+            <div
+              className={`bg-white w-6 h-6 rounded-full shadow-md ${
+                dark ? `transform translate-x-4 bg-gray-200` : ``
+              } `}
+            ></div>
+          </div>
+        </div>
+
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -35,8 +76,20 @@ function App() {
           >
             Submit
           </button>
-          <Data data={weatherData} />
         </form>
+
+        {showData ? <Data data={weatherData} /> : undefined}
+
+        <footer className="my-4 p-2 text-center">
+          Weather data from{" "}
+          <a
+            href="https://openweathermap.org/"
+            className="text-blue-400 hover:text-blue-500"
+          >
+            Openweather
+          </a>{" "}
+          API
+        </footer>
       </div>
     </div>
   );
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,10 +1,21 @@
 const Data = ({ data }) => {
-  const { name, timezone } = data;
+  const { coord, weather, wind, main, name } = data;
   return (
     <div className="my-8 mx-2 p-4 bg-white dark:bg-gray-700 rounded-lg shadow flex flex-col justify-center items-center">
-      <h2>Data</h2>
-      <h2 className="text-xl font-bold">{name}</h2>
-      <h2>{timezone}</h2>
+      <p className="text-lg font-semibold">{name}</p>
+      <p>
+        ( {coord.lon}, {coord.lat} )
+      </p>
+      <p className="mt-2 text-2xl font-bold">{weather[0].main}</p>
+      <p className="mb-2 capitalize text-lg font-medium">
+        {weather[0].description}
+      </p>
+      <p>
+        Temperature: <b>{main.temp} C</b>, humidity: <b>{main.humidity} %</b>
+      </p>
+      <p>
+        Wind Speed: <b>{wind.speed} m/s</b>
+      </p>
     </div>
   );
 };
